2f7e338f787e8c813ae5dc0ae145bdecf3206912
Vishnu Medhini <vishnumedhini@gmail.com>
2026-07-20 22:29:46 +0530

    new component

--- React-app/src/Course.jsx
+++ React-app/src/Course.jsx
@@ -3,7 +3,6 @@ let course1 = "HTML"
 function Course(){
     return(
         <div className="card">
-            <img src=" " alt=" " />
             <h3>{course1}</h3>
             <p>this is an html course</p>
         </div>
