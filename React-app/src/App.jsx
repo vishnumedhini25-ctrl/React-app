c258e726f5cb07c9fc172bc0be9ff7cc9250c724
Vishnu Medhini <vishnumedhini@gmail.com>
2026-07-21 21:20:30 +0530

    components,props,condi Render

--- React-app/src/App.jsx
+++ React-app/src/App.jsx
@@ -2,14 +2,19 @@ import './App.css'
 import Course from './Course'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import html from './assets/Html.png'
+import css from './assets/css.png'
+import js from './assets/javaScript-logo.png';
+
 
 function App() {
 
   return (
     <>
-    <Navbar/>
-    <Course/>
-    <Footer/>
+    <Course name="HTML" price="199" image={html} show={true}/>
+    <Course name="CSS" price="199" image={css} show={true}/>
+    <Course name="JS" price="299" image={js}/>
+    
     </>
       
   )
--- React-app/src/Course.jsx
+++ React-app/src/Course.jsx
@@ -1,11 +1,27 @@
-let course1 = "HTML"
-
-function Course(){
-    return(
+import propTypes from 'prop-types'
+function Course(props){
+    if(props.show == true){
+        return(
         <div className="card">
-            <h3>{course1}</h3>
-            <p>this is an html course</p>
+            <img src={props.image} rel=""></img>
+            <h3>{props.name}</h3>
+            <p>{props.price}</p>
+
         </div>
     )
+
+    }
+    else{
+        return(
+            <div className="card">
+                Course is not available
+            </div>
+        )
+    }
 }
+Course.propTypes = {
+    name : propTypes.string,
+    show : propTypes.bool
+}
+
 export default Course
