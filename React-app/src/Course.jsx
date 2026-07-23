47aa28320b02a629b8631cd16e0b217612b72925
Vishnu Medhini <vishnumedhini@gmail.com>
2026-07-23 22:17:35 +0530

    click Event,State Hooks

--- React-app/src/Course.jsx
+++ React-app/src/Course.jsx
@@ -1,10 +1,25 @@
 import propTypes from 'prop-types'
+import { useState } from 'react'
+
+
+
 function Course(props){
+    const [purchase,setpurchase] = useState(false)
+
+    function BuyCourse(Discount){
+        console.log(props.name,"Purchased",Discount,"%Discount")
+        setpurchase(true)
+        console.log(purchase)
+    }
         return(
         <div className="card">
             <img src={props.image} rel=""></img>
             <h3>{props.name}</h3>
             <p>{props.price}</p>
+            <button onClick={()=>BuyCourse(10)}>Buy Now</button>
+            <p>
+                {purchase ? "Already Purchase" : "Get It Now"}
+            </p>
 
         </div>
         )
